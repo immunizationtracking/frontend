ac9c6bd2a9a1cf879aed700dbe4efdd1de61434d
omar okla <omaro@me.com>
2019-04-16 13:43:57 -0400

    fixed and reimported reducer, action, and logUserIn function. working login flow

--- immunotracker/src/actions/index.js
+++ immunotracker/src/actions/index.js
@@ -7,3 +7,15 @@ export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
 export const REGISTER_POST_START = "REGISTER_POST_START";
 export const REGISTER_POST_SUCCESS = "REGISTER_POST_SUCCESS";
 export const REGISTER_POST_FAILURE = "REGISTER_POST_FAILURE";
+
+export const logUserIn = user => dispatch => {
+	dispatch({ type: LOG_IN_START });
+	axios
+		.post("API-URL-Here", user)
+		.then(res => {
+			dispatch({ type: LOG_IN_SUCCESS, payload: res.data });
+		})
+		.catch(err => {
+			dispatch({ type: LOG_IN_FAILURE, payload: err.err });
+		});
+};

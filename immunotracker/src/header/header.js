b6df8005542fccde05d03b5484e6c07907236df5
omar okla <omaro@me.com>
2019-04-18 16:08:07 -0400

    up to date

--- immunotracker/src/header/header.js
+++ immunotracker/src/header/header.js
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
 import "./header.css";
 import banner from "../img/Banner.png";
+import { connect } from "react-redux";
+import { logUserOut } from "../actions";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        <img src={banner} className="logo" />
+class Header extends Component {
+	constructor() {
+		super();
+	}
 
-        <div className="link">
-          <a href="/">Log In</a>
-          <a href="/register">Sign Up</a>
-        </div>
-      </div>
-    );
-  }
+	logOut = e => {
+		this.props.logUserOut();
+	};
+
+	render() {
+		return (
+			<div className='Header'>
+				<img src={banner} className='logo' />
+
+				<div className='link'>
+					{this.props.isLoggedIn ? (
+						<a onClick={this.logOut} href='/'>
+							Log Out
+						</a>
+					) : (
+						<a href='/'>Log In</a>
+					)}
+					<a href='/register'>Sign Up</a>
+				</div>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = state => {
+	return {
+		isLoggedIn: state.isLoggedIn,
+		isLoggedOut: state.isLoggedOut
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	{ logUserOut }
+)(Header);
